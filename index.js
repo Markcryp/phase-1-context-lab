469da7d8ea6cf0b36c7eab9eb96a1808b8820581
Mark <mark.njau@student.moringaschool.com>
2024-07-22 16:53:05 +0300

    second commit

--- index.js
+++ index.js
@@ -99,15 +99,15 @@ function calculatePayroll(employeeRecords) {
  for you to use if you need it!
  */
 
-// const allWagesFor = function () {
-//     const eligibleDates = this.timeInEvents.map(function (e) {
-//         return e.date
-//     })
+const allWagesFor = function () {
+    const eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
 
-//     const payable = eligibleDates.reduce(function (memo, d) {
-//         return memo + wagesEarnedOnDate.call(this, d)
-//     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    const payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-//     return payable
-// }
+    return payable
+}
 
